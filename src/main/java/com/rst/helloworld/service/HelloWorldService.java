f9c3515777ea71819fb25f4ba2933eb787f28f96
yathish12 <103912830+yathish12@users.noreply.github.com>
2023-06-09 09:25:24 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Hello, Welcome to DevOps Hackathon";
 		}else{
 			return "Hello " + name;
 		}
